03fbb42027279fee3aed587e9d063fa106e207eb
Pratik Yawalkar <pratik26yawalkar@gmail.com>
2025-07-01 21:55:26 +0530

    update dow

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/NavBar';
 import Hero from '@/sections/Hero'
 import ShowcaseSection from '@/sections/ShowcaseSection'
 // import LogoShowcase from '@/sections/LogoShowcase'
