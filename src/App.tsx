2a10edbce149855dbb21acd3c138e5f8a7dd2bba
Pratik Yawalkar <pratik26yawalkar@gmail.com>
2025-07-01 21:47:20 +0530

    updat related to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Navbar from './components/Navbar';
+import Navbar from '@/components/Navbar';
 import Hero from '@/sections/Hero'
-import ShowcaseSection from './sections/ShowcaseSection'
-// import LogoShowcase from './sections/LogoShowcase'
-import FeatureCards from './sections/FeatureCards'
-import Experience from './sections/Experience'
-import TechStack from './sections/TechStack'
-// import Testimonials from './sections/Testimonials'
-import Contact from './sections/Contact'
-import Footer from './sections/Footer'
+import ShowcaseSection from '@/sections/ShowcaseSection'
+// import LogoShowcase from '@/sections/LogoShowcase'
+import FeatureCards from '@/sections/FeatureCards'
+import Experience from '@/sections/Experience'
+import TechStack from '@/sections/TechStack'
+// import Testimonials from '@/sections/Testimonials'
+import Contact from '@/sections/Contact'
+import Footer from '@/sections/Footer'
 
 const App = () => {
   return (
